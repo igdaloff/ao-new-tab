a19c42c026554281b76c2db212f1e19f62bbaf04
igdaloff <louis.nathan@gmail.com>
2018-04-25 23:47:46 -0400

    got image and text to load for Place of The Day on AO homepage; still doesn't work as a Chrome extension, only locally

--- js/app.js
+++ js/app.js
@@ -14,47 +14,24 @@ fetch('/js/urls.json').then((res) => {
 			}
 			fetch(url).then((res) => {
 				res.text().then((htmlTxt) => {
-					/* Extract the RSS Feed URL from the website */
 					try {
 						let doc = DOMPARSER(htmlTxt, 'text/html')
-						var feedUrl = doc.querySelector('link[type="application/rss+xml"]').href
-						var placeOfDayElement = doc.querySelector('.hp-item-place');
-						var placeOfDayImage = placeOfDayElement.querySelector('img').src;
-						console.log(placeOfDayImage);
-						$("#main-image").attr("src", placeOfDayImage); //TRY AND GET THIS IMAGE TO DISPLAY. I THINK THIS LINE OF CODE IS IN THE WRONG PLACE.
+						var placeOfDayElement = doc.querySelector('.place-of-the-day-card');
+						var placeOfDayImage = placeOfDayElement.querySelector('img');
+						var placeOfDayImageSrc = placeOfDayImage.getAttribute('data-src');
+						var placeOfDayName = placeOfDayElement.querySelector('.place-of-the-day-card .js-title-content');
+						var placeOfDayDescription = placeOfDayElement.querySelector('.place-of-the-day-card .js-subtitle-content');
+						var placeOfDayUrl = placeOfDayElement.getAttribute('href');
+
+						$('.page-container').css('background-image', 'url(' + placeOfDayImageSrc + ')');
+						$('h1').html(placeOfDayName);
+						$('h2').html(placeOfDayDescription);
+						$('.page-container').attr('href', 'https://atlasobscura.com' + placeOfDayUrl);
+
 					} catch (e) {
-						console.error('Error in parsing the website');
+						console.error('Error in parsing the site');
 						return
 					}
-					/* Fetch the RSS Feed */
-					// fetch(feedUrl).then((res) => {
-					// 	res.text().then((xmlTxt) => {
-					// 		/* Parse the RSS Feed and display the content */
-					// 		try {
-					// 			let doc = DOMPARSER(xmlTxt, "text/xml")
-					// 			let heading = document.createElement('h1')
-					// 			heading.textContent = url.hostname
-					// 			frag.appendChild(heading)
-					// 			doc.querySelectorAll('item').forEach((item) => {
-					// 				let temp = document.importNode(document.querySelector('template').content, true);
-					// 				let i = item.querySelector.bind(item)
-					// 				let t = temp.querySelector.bind(temp)
-					// 				t('h2').textContent = !!i('title') ? i('title').textContent : '-'
-					// 				t('a').textContent = t('a').href = !!i('link') ? i('link').textContent : '#'
-					// 				t('h3').textContent = url.hostname
-
-					// 				frag.appendChild(temp)
-					// 			})
-					// 		} catch (e) {
-					// 			console.error('Error in parsing the feed')
-					// 		}
-					// 		if(hasBegun) {
-					// 			document.querySelector('output').textContent = '';
-					// 			hasBegun = false;
-					// 		}
-					// 		document.querySelector('output').appendChild(frag)
-					// 	})
-					// }).catch(() => console.error('Error in fetching the RSS feed'))
 				})
 			}).catch(() => console.error('Error in fetching the website'))
 		})
