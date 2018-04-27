509a36baaabe05cc5866747b0430dec7be21921f
igdaloff <louis.nathan@gmail.com>
2018-04-27 00:17:38 -0400

    getting slightly larger image from place page; done for now

--- js/app.js
+++ js/app.js
@@ -16,17 +16,21 @@ fetch('/js/urls.json').then((res) => {
 				res.text().then((htmlTxt) => {
 					try {
 						let doc = DOMPARSER(htmlTxt, 'text/html')
+
 						var placeOfDayElement = doc.querySelector('.place-of-the-day-card');
-						var placeOfDayImage = placeOfDayElement.querySelector('img');
-						var placeOfDayImageSrc = placeOfDayImage.getAttribute('data-src');
 						var placeOfDayName = placeOfDayElement.querySelector('.place-of-the-day-card .js-title-content');
 						var placeOfDayDescription = placeOfDayElement.querySelector('.place-of-the-day-card .js-subtitle-content');
 						var placeOfDayUrl = placeOfDayElement.getAttribute('href');
 
-						$('.page-container').css('background-image', 'url(https://' + placeOfDayImageSrc + ')');
+						$.get('https://atlasobscura.com' + placeOfDayUrl, function(response) {
+				  		var mainImage = $(response).find('.gallery-image-container')[0];
+				  		var mainImageUrl = $(mainImage).attr('data-lightbox-src');
+				  		$('.page-container').css('background-image', 'url(https:' + mainImageUrl + ')');
+						});
+
 						$('h1').html(placeOfDayName);
 						$('h2').html(placeOfDayDescription);
-						$('.page-container').attr('href', 'https://atlasobscura.com' + placeOfDayUrl);
+						$('.text-container').attr('href', 'https://atlasobscura.com' + placeOfDayUrl);
 
 					} catch (e) {
 						console.error('Error in parsing the site');
